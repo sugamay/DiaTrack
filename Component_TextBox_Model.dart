37d5110e226117c9f1fb5f974f03e2b5d94a1d17
sugamay <158982792+sugamay@users.noreply.github.com>
2024-04-07 13:50:47 -0700

    Added pair device and text functionality

---
+++ Component_TextBox_Model.dart
@@ -0,0 +1,16 @@
+// Component text box Model
+
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'chip_widget.dart' show ChipWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class ChipModel extends FlutterFlowModel<ChipWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
---
+++ PairDevice(MODEL).dart
@@ -0,0 +1,28 @@
+// Pair Device MODEL
+
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'pair_device_widget.dart' show PairDeviceWidget;
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+
+class PairDeviceModel extends FlutterFlowModel<PairDeviceWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+  }
+}
